130b59d41a17dfbb1daf07ee5653029478ad989a
zhengyu_dai <zhengyu_dai@foxmail.com>
2021-07-20 18:37:16 +0800

    commit from company

--- gulimall-test-sso-client1/src/main/java/com/dzy/gulimall/ssoclient/controller/SsoController.java
+++ gulimall-test-sso-client1/src/main/java/com/dzy/gulimall/ssoclient/controller/SsoController.java
@@ -21,11 +21,15 @@ public class SsoController {
 
     @GetMapping("/employees")
     public String employees(Model model, HttpSession session, @RequestParam(value = "token", required = false) String token) {
+        //判断请求中是否带有token，如果是从认证服务器回调的，就是登录过的并且会带有token，可以通过
+        //token去认证服务器拿到用户信息
         if(StringUtils.hasText(token)) {
             ResponseEntity<String> entity = restTemplate.getForEntity("http://ssoserver.com:8080/userinfo?token=" + token, String.class);
             String userInfo = entity.getBody();
+            //将拿到的用户信息放入自己系统的session中
             session.setAttribute("loginUser", userInfo);
         }
+        //如果自身系统的session中没有用户信息，重定向到认证服务器进行登录，并且带上登录成功之后的回调地址。
         if(session.getAttribute("loginUser") == null) {
             return "redirect:http://ssoserver.com:8080/login.html?redirect_url=http://client1.com:8081/employees";
         }
--- gulimall-test-sso-server/src/main/java/com/dzy/gulimall/ssoserver/controller/LoginController.java
+++ gulimall-test-sso-server/src/main/java/com/dzy/gulimall/ssoserver/controller/LoginController.java
@@ -24,9 +24,12 @@ public class LoginController {
     @GetMapping("/login.html")
     public String loginPage(@RequestParam("redirect_url") String redirectUrl, Model model,
                             @CookieValue(value = "sso_token", required = false) String token) {
+        //跳转登录页的时候，先判断一下cookie中有没有sso_token的值，如果有的话，说明用户之前在其他系统登录过，
+        //直接重定向到回调的地址并且拼上token，让请求登录的系统拿到token值来获取用户信息。
         if(StringUtils.hasText(token)) {
             return "redirect:" + redirectUrl + "?token=" +token;
         }
+        //如果没有token的值，就去到登录页面进行登录
         model.addAttribute("redirectUrl", redirectUrl);
         return "login";
     }
@@ -34,7 +37,8 @@ public class LoginController {
     @PostMapping("/login")
     public String login(String username, String password, String redirectUrl, HttpServletResponse response) {
         if(StringUtils.hasText(username) && StringUtils.hasText(password)) {
-            //登录成功，将登录的数据存到redis，并且在cookie中留下痕迹
+            //登录成功，将登录的数据存到redis，并且在cookie中留下痕迹，方便下次用户访问一个未登录的系统，
+            //跳转到认证服务器时能够带上这个cookie。
             String token = UUID.randomUUID().toString().replace("-", "");
             redisTemplate.opsForValue().set(token, username);
             Cookie cookie = new Cookie("sso_token", token);
