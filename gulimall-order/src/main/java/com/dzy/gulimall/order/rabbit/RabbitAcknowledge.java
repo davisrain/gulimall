b6a9e37412fa5c5f9b36e68d8e44bc1e8d862d99
zhengyu_dai <zhengyu_dai@foxmail.com>
2021-07-23 21:50:29 +0800

    commit from home

--- gulimall-order/src/main/java/com/dzy/gulimall/order/rabbit/RabbitAcknowledge.java
+++ gulimall-order/src/main/java/com/dzy/gulimall/order/rabbit/RabbitAcknowledge.java
@@ -6,16 +6,23 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 
 @Component
 public class RabbitAcknowledge {
 
     @RabbitListener(queues = "hello-java-queue")
-    public void receiveMessageByManualAck(Message message, Channel channel) {
+    public void receiveMessageByManualAck(Message message, Channel channel) throws IOException {
+        channel.basicQos(1);
         byte[] body = message.getBody();
         long deliveryTag = message.getMessageProperties().getDeliveryTag();
         System.out.println("获取到消息：" + new String(body));
+//        try {
+//            TimeUnit.SECONDS.sleep(30);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         try {
             if(deliveryTag % 2 == 0) {
                 channel.basicAck(deliveryTag, false);
